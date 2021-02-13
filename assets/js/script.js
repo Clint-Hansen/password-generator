b028503de7082c93efcfcefe50e801adf7e244bb
Clint Hansen <clint.hansen2009@gmail.com>
2021-02-13 01:09:20 -0700

    added comments for clarity

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,9 @@ function writePassword() {
   passwordText.value = password;
 };
 
+
 function generatePassword() {
+  // password length is set with prompt using while() to check for null and out of range values.
   let passwordLength = "";
   while (passwordLength === ""
   || passwordLength === "null"
@@ -25,29 +27,34 @@ function generatePassword() {
     passwordLength = prompt("Please enter a password length between 8 and 128 characters long.");
     alert("You have set your password length to be " + passwordLength + " characters long.")
   } 
-  
+  // character type selections
   let charType = [];
-  
+  // confirm to include lowercase characters. If true, validation alert triggers.
   let lowercaseConfirm = confirm("Would you like to include lowercase characters in your password?");
   if (lowercaseConfirm) {
     charType = charType.concat(lowercaseArray);
     alert("You have selected to include lowercase characters in your password.");
   }
+  // confirm to include uppercase characters. If true, validation alert triggers.
   let uppercaseConfirm = confirm("Would you like to include uppercase characters in your password?");
   if (uppercaseConfirm) {
     charType = charType.concat(uppercaseArray);
     alert("You have selected to include uppercase characters in your password.");
   }
+  // confirm to include number characters. If true, validation alert triggers.
   let numbersConfirm = confirm("Would you like to include numbers in your password?");
   if (numbersConfirm) {
     charType = charType.concat(numbersArray);
     alert("You have selected to include numbers in your password.");
   }
+  // confirm to include symbol characters. If true, validation alert triggers.
   let symbolsConfirm = confirm("Would you like to include symbols in your password?");
   if (symbolsConfirm) {
     charType = charType.concat(symbolsArray);
     alert("You have selected to include symbols in you password.")
   }
+  // character selections from charType variable are passed through the for() loop using the passwordLength integer
+  // the charType items are randomized and then pushed to the passwordArray
   let passwordArray = [];
   for(i = 0; i < passwordLength; i++) {
     let passwordChars = charType[Math.floor(Math.random() * charType.length)]
